f9337f08a892d19536c67774d627c3df58b83577
vinothini <vinothiniciet@gmail.com>
2024-11-07 17:12:00 +0400

    create basic settings for youtube project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Body from "./Components/Body";
+import Head from "./Components/Head";
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-700">
+        Namaste react
+        <Head />
+        <Body />
+      </h1>
+    </div>
+    
+   
+  );
+}
+
+export default App;
